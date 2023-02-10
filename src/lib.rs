9fd3421d6ddb1942976d816b291f9b7fbb383f5c
kallebysantos <kalleby_santos@hotmail.com>
2023-02-10 22:10:10 +0000

    📦 NEW: mounting nested routes from Meteoritus

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
-use rocket::fairing::Fairing;
+#[macro_use]
+extern crate rocket;
 
-struct Meteoritus {}
+use rocket::{
+    fairing::{self, Fairing, Info, Kind},
+    Build, Rocket,
+};
 
+#[derive(Default, Clone)]
+pub struct Meteoritus {}
+
+#[rocket::async_trait]
 impl Fairing for Meteoritus {
-    fn info(&self) -> rocket::fairing::Info {
-        todo!()
+    fn info(&self) -> Info {
+        Info {
+            name: "Meteoritus",
+            kind: Kind::Ignite,
+        }
+    }
+
+    async fn on_ignite(&self, rocket: Rocket<Build>) -> fairing::Result {
+        #[get("/")]
+        fn hello_meteor() -> &'static str {
+            "Hello from Meteoritus"
+        }
+
+        let meteor_build = rocket
+            .manage(self.clone())
+            .mount("/meteor", routes![hello_meteor]);
+
+        Ok(meteor_build)
     }
 }
