0b1b1677f1e9f9b4431a9a07a5b4b824ea33fa27
kallebysantos <kalleby_santos@hotmail.com>
2023-02-10 21:39:14 +0000

    📦 Creating base of Meteoritus struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use rocket::fairing::Fairing;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Meteoritus {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Fairing for Meteoritus {
+    fn info(&self) -> rocket::fairing::Info {
+        todo!()
     }
 }
