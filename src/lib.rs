3d152ab3eb15291fe8610da6340af3456a4d2ccf
kallebysantos <kalleby_santos@hotmail.com>
2023-03-14 13:30:37 +0000

    📖 DOC: Adding library documentation entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,66 @@
+#![doc(
+    html_logo_url = "https://github.com/kallebysantos/meteoritus/raw/main/assets/logo-boxed-rounded.png"
+)]
+#![doc(
+    html_favicon_url = "https://github.com/kallebysantos/meteoritus/raw/main/assets/favicon.ico"
+)]
 #![feature(trait_alias)]
 #![feature(file_create_new)]
 #![feature(type_changing_struct_update)]
+
+//!  # Meteoritus - API Documentation
+//!
+//! Hello, and welcome to the Meteoritus API documentation!
+//!
+//! ## Usage
+//!
+//! Meteoritus is a [`Fairing`] that implements tus protocol on top of [`Rocket`] framework, so in
+//! order to use it you'll need the following dependencies in `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! rocket = "0.5.0-rc.2"
+//! meteoritus = "0.1.0"
+//! ```
+//!
+//! Then attach [`Meteoritus`] to your [`Rocket`] server on launch:
+//!
+//! ```rust,no_run
+//! #[macro_use]
+//! extern crate rocket;
+//! use rocket::data::ByteUnit;
+//! use meteoritus::Meteoritus;
+//!
+//! #[get("/")]
+//! fn hello() -> &'static str {
+//!     "Hello, world!"
+//! }
+//!
+//! #[launch]
+//! fn rocket() -> _ {
+//!     let meteoritus = Meteoritus::new()
+//!         .mount_to("/api/files")
+//!         .with_temp_path("./tmp/uploads")
+//!         .with_max_size(ByteUnit::Gibibyte(1))
+//!         .on_creation(|rocket, file, upload_uri| {
+//!              println!("File created: {:?}", file);
+//!              Ok(())
+//!          })
+//!         .on_complete(|rocket| {
+//!              println!("Upload complete!");
+//!          })
+//!         .build();
+//!
+//!     rocket::build()
+//!         .attach(meteoritus)
+//!         .mount("/", routes![hello])
+//! }
+//! ```
+//! [`Rocket`]: https://api.rocket.rs/v0.5-rc/rocket/index.html
+//! [`Fairing`]: https://api.rocket.rs/v0.5-rc/rocket/fairing/index.html
+
+/// These are public dependencies! Update docs if these are changed, especially
+/// figment's version number in docs.
 
 #[macro_use]
 extern crate rocket;
