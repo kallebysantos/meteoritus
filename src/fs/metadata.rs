1ca3fdc1d451125c9719f88c916606b846339d11
kallebysantos <kalleby_santos@hotmail.com>
2023-03-17 23:09:55 +0000

    📦 NEW: Get raw values from metadata

--- src/fs/metadata.rs
+++ src/fs/metadata.rs
@@ -1,20 +1,40 @@
+use base64::Engine as _;
 use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct Metadata(HashMap<String, String>);
 
+#[derive(Debug, PartialEq)]
+pub enum MetadataError {
+    InvalidKey,
+    DecodeError(String),
+    InvalidMetadataFormat,
+}
+
 impl Metadata {
     pub fn new() -> Self {
         Default::default()
     }
+
+    pub fn get_raw(&self, key: &str) -> Result<Vec<u8>, MetadataError> {
+        let value = match self.0.get(key) {
+            Some(v) => v,
+            None => return Err(MetadataError::InvalidKey),
+        };
+
+        match base64::engine::general_purpose::STANDARD.decode(value) {
+            Ok(decoded) => Ok(decoded),
+            Err(e) => Err(MetadataError::DecodeError(e.to_string())),
+        }
+    }
 }
 
 impl TryFrom<&str> for Metadata {
-    type Error = &'static str;
+    type Error = MetadataError;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         if value.is_empty() {
-            return Err("Metadata cannot be empty");
+            return Err(MetadataError::InvalidMetadataFormat);
         }
 
         let mut metadata = Metadata::new();
@@ -28,12 +48,21 @@ impl TryFrom<&str> for Metadata {
 
             let parts: Vec<&str> = pair.split(' ').map(|v| v.trim()).collect();
 
-            if parts.len() != 2 || parts[0].is_empty() || parts[1].is_empty() {
-                return Err("Invalid metadata format");
+            if parts.is_empty() || parts.len() > 2 {
+                return Err(MetadataError::InvalidMetadataFormat);
             }
 
-            if let [key, value] = parts[..] {
-                metadata.0.insert(key.to_string(), value.to_string());
+            if parts[0].is_empty() {
+                return Err(MetadataError::InvalidKey);
+            }
+
+            if let (Some(key), value) = (parts.get(0), parts.get(1)) {
+                let value = match value {
+                    Some(v) => v.to_string(),
+                    None => String::default(),
+                };
+
+                metadata.0.insert(key.to_string(), value);
             }
         }
 
@@ -43,16 +72,18 @@ impl TryFrom<&str> for Metadata {
 
 #[cfg(test)]
 mod tests {
+    use std::str::from_utf8;
+
     use super::*;
 
     const METADATA_STR: &str =
-        "relativePath bnVsbA==, filename bXlfdmlkZW8ubXA0, filetype dmlkZW8vbXA0";
+        "relativePath bnVsbA==, filename bXlfdmlkZW8ubXA0, filetype dmlkZW8vbXA0,is_confidential";
 
     #[test]
     fn valid_from_str() {
         let metadata = Metadata::try_from(METADATA_STR).unwrap();
 
-        assert_eq!(metadata.0.len(), 3);
+        assert_eq!(metadata.0.len(), 4);
         assert_eq!(
             metadata.0.get("relativePath"),
             Some(&String::from("bnVsbA=="))
@@ -72,7 +103,7 @@ mod tests {
         let metadata = Metadata::try_from("");
 
         assert!(metadata.is_err());
-        assert_eq!(metadata.err(), Some("Metadata cannot be empty"));
+        assert_eq!(metadata.err(), Some(MetadataError::InvalidMetadataFormat));
     }
 
     #[test]
@@ -80,6 +111,28 @@ mod tests {
         let metadata = Metadata::try_from("foobar, fas bars foo bar, ");
 
         assert!(metadata.is_err());
-        assert_eq!(metadata.err(), Some("Invalid metadata format"));
+        assert_eq!(metadata.err(), Some(MetadataError::InvalidMetadataFormat));
+    }
+
+    #[test]
+    fn get_raw_value_successfully() {
+        let metadata = Metadata::try_from(METADATA_STR).unwrap();
+
+        assert_eq!(metadata.get_raw("filetype"), Ok(b"video/mp4".to_vec()));
+        assert_eq!(metadata.get_raw("filename"), Ok(b"my_video.mp4".to_vec()));
+        assert_eq!(
+            from_utf8(&metadata.get_raw("relativePath").unwrap()),
+            Ok("null")
+        );
+    }
+
+    #[test]
+    fn get_raw_value_error() {
+        let metadata = Metadata::try_from(METADATA_STR).unwrap();
+
+        assert_eq!(
+            metadata.get_raw("foo bar bars"),
+            Err(MetadataError::InvalidKey)
+        );
     }
 }
