076a0d0dd8e4bf3805c21e12bcaede07ae7065ed
kallebysantos <kalleby_santos@hotmail.com>
2023-03-28 11:08:55 +0100

    📦 NEW: impl. build file method for Local Vault struct

--- src/fs/vault.rs
+++ src/fs/vault.rs
@@ -29,3 +29,31 @@ impl LocalVault {
         Self { save_path }
     }
 }
+
+impl Vault for LocalVault {
+    fn build_file(
+        &self,
+        length: u64,
+        metadata: Option<&str>,
+    ) -> Result<FileInfo<Built>, VaultError> {
+        let metadata = match metadata {
+            Some(metadata) => match Metadata::try_from(metadata) {
+                Ok(m) => m,
+                Err(e) => return Err(VaultError::CreationError(Box::new(e))),
+            },
+
+            None => Metadata::default(),
+        };
+
+        let file_info = FileInfo::new(length)
+            .with_uuid()
+            .with_metadata(metadata)
+            .build();
+
+        Ok(file_info)
+    }
+
+    fn create_file(&self, file_info: FileInfo<Built>) -> Result<FileInfo<Created>, VaultError> {
+        todo!()
+    }
+}
